891694627e77faa114d998415a10ccad5da46922
Tiago Vinícius de Oliveira Brachini <tvobrachini@gmail.com>
2014-09-16 13:47:32 -0300

    sending changes in views.py

--- app1/app1/urls.py
+++ app1/app1/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
-from app1.views import olaMundo, dataAtual
+from app1.views import olaMundo, dataAtual, dataMais
 admin.autodiscover()
 
 urlpatterns = patterns(
@@ -10,5 +10,6 @@ urlpatterns = patterns(
     url(r'^olaMundo/', olaMundo),
     url(r'^dataAtual/', dataAtual),
     url(r'^$', olaMundo),
+    url(r'^data/(\d+)/$', dataMais),
     url(r'^admin/', include(admin.site.urls)),
 )
--- app1/app1/views.py
+++ app1/app1/views.py
@@ -10,3 +10,13 @@ def dataAtual(request):
     now = datetime.datetime.now()
     html = "<em>Agora é %s.</em>" % now
     return HttpResponse(html)
+
+
+def dataMais(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    data = datetime.datetime.now() + datetime.timedelta(days=offset)
+    html = "<em>Em %s dia(s), será %s.</em>" % (offset, data)
+    return HttpResponse(html)
